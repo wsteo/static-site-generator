d2669aa09b21e9963bb225f1f6aaafb09e32ca5e
wsteo <35697328+wsteo@users.noreply.github.com>
2025-01-15 16:11:52 +0800

    update implementation

---
+++ src/nodeconverter.py
@@ -0,0 +1,20 @@
+from textnode import *
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    if delimiter not in ["`", "*", "**"]:
+        raise Exception("that is not an invalid markdown syntax")
+
+    for node in old_nodes:
+        if delimiter in node.text and node.text_type == TextType.TEXT:
+            text_list = node.text.split(delimiter)
+            for i in range(len(text_list)):
+                if i % 2 == 1:
+                    new_nodes.append(TextNode(text_list[i], text_type))
+                else:
+                    new_nodes.append(TextNode(text_list[i], TextType.TEXT))
+        else:
+            new_nodes.append(node)
+
+    return new_nodes
---
+++ src/test_nodeconverter.py
@@ -0,0 +1,63 @@
+import unittest
+
+from nodeconverter import *
+
+
+class TestNodeConverter(unittest.TestCase):
+    def test_split_delimiter_code_block(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        output = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        assert split_nodes_delimiter([node], "`", TextType.CODE) == output
+
+    def test_split_delimiter_bold_block(self):
+        node = TextNode("This is text with a **bold block** word", TextType.TEXT)
+        output = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
+        assert split_nodes_delimiter([node], "**", TextType.BOLD) == output
+
+    def test_split_delimiter_italic_block(self):
+        node = TextNode("This is text with a *italic block* word", TextType.TEXT)
+        output = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("italic block", TextType.ITALIC),
+            TextNode(" word", TextType.TEXT),
+        ]
+        assert split_nodes_delimiter([node], "*", TextType.ITALIC) == output
+
+    def test_split_delimiter_code_multiple(self):
+        node1 = TextNode("This is text with a `code block` word", TextType.TEXT)
+        node2 = TextNode("This is text with a **bold block** word", TextType.TEXT)
+        node3 = TextNode("This is text with a *italic block* word", TextType.TEXT)
+
+        new_nodes_list = split_nodes_delimiter(
+            [node1, node2, node3], "`", TextType.CODE
+        )
+        new_nodes_list = split_nodes_delimiter(new_nodes_list, "**", TextType.BOLD)
+        new_nodes_list = split_nodes_delimiter(new_nodes_list, "*", TextType.ITALIC)
+        output = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("italic block", TextType.ITALIC),
+            TextNode(" word", TextType.TEXT),
+        ]
+        assert new_nodes_list == output
+
+    def test_invalid_markdown_syntax(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        self.assertRaises(Exception, split_nodes_delimiter, [node], ".", TextType.CODE)
+
+
+if __name__ == "__main__":
+    unittest.main()
